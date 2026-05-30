3f9ae79d6dd7fb474f03aaa070d8fad8d137f357
Jordan Kniest <mail@jkniest.de>
2026-05-30 21:05:02 +0200

    Render ducks!

--- internal/orchestrator/apps.go
+++ internal/orchestrator/apps.go
@@ -7,16 +7,23 @@ import (
 	"sync"
 
 	"github.com/therealironduck/flufu/internal/agent"
+	"github.com/therealironduck/flufu/internal/pet"
 )
 
 func Start(ctx context.Context) {
 	var wg sync.WaitGroup
+	cancelCtx, cancel := context.WithCancel(ctx)
 
 	wg.Go(func() {
 		if err := agent.Spawn(ctx); err != nil {
 			fmt.Fprintf(os.Stderr, "failed to run agent: %v", err)
 			os.Exit(1)
 		}
+		cancel()
+	})
+
+	wg.Go(func() {
+		pet.Render(cancelCtx)
 	})
 
 	wg.Wait()
