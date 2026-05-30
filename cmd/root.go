2b680741e4d819578ceb42725c36b281a7ff5a4c
Jordan Kniest <mail@jkniest.de>
2026-05-30 17:50:17 +0200

    Initial

---
+++ cmd/root.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "flufu",
+	Short: "Wrapper for AI coding assistants that add a small coding buddy",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Hello World!")
+	},
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// TODO
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/therealironduck/flufu/cmd"
+
+func main() {
+	cmd.Execute()
+}
